2fa7eebd57de02d85285d8dbaab049ec7a4fae68
nmshohel <nmshohel1992@gmail.com>
2022-09-11 13:01:40 +0600

    'shohel'

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dteuxtf.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
